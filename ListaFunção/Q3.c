954b6fe7b3c1e0fd8c3f862de6ae6e2d0310d0a7
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-05-07 10:35:45 -0300

    Update Q3.c

--- ListaFunção/Q3.c
+++ ListaFunção/Q3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int fatorial(int n) {
+    if (n < 0) {
+        return -1;
+    }
+
+    int i, resultado = 1;
+    for (i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+
+    return resultado;
+}
+
+int main() {
+    int numero, resultado;
+
+    scanf("%d", &numero);
+    resultado = fatorial(numero);
+
+    if (resultado == -1) {
+        printf("Erro: não é possível calcular o fatorial de número negativo.\n");
+    } else {
+        printf("%d\n", resultado);
+    }
+
+    return 0;
+}
