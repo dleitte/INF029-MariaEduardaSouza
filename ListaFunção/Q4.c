485c753e5ad8b9f3fb32387c431ca165659f6b83
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-05-07 10:41:39 -0300

    Update Q4.c

--- ListaFunção/Q4.c
+++ ListaFunção/Q4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void ler3Numeros(int numeros[3]) {
+    for (int i = 0; i < 3; i++) {
+        printf("Digite o %dº número: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+}
+
+int main() {
+    int numeros[3];
+    ler3Numeros(numeros);
+    
+    printf("Os três números são: ");
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", numeros[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
